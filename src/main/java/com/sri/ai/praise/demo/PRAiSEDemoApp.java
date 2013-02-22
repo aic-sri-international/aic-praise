355e31c36cad35063b81d2b20688fcec1ff2809c
ctjoreilly <ctjoreilly@e74e62f3-1872-5a1b-a7a8-c7220c51b9cb>
2013-02-22 22:03:21 +0000

    - adding skeleton of new demo app
    git-svn-id: https://aic-praise.googlecode.com/svn/trunk@483 e74e62f3-1872-5a1b-a7a8-c7220c51b9cb

---
+++ src/main/java/com/sri/ai/praise/demo/PRAiSEDemoApp.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2013, SRI International
+ * All rights reserved.
+ * Licensed under the The BSD 3-Clause License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the aic-expresso nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sri.ai.praise.demo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+import com.google.common.annotations.Beta;
+import com.sri.ai.grinder.GrinderConfiguration;
+import com.sri.ai.grinder.demo.OutputPanel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JSplitPane;
+import java.awt.Dimension;
+
+/**
+ * 
+ * @author oreilly
+ *
+ */
+@Beta
+public class PRAiSEDemoApp {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					String configuredLookAndFeel = GrinderConfiguration.getDemoAppDefaultLookAndFeel();
+				    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				        if (configuredLookAndFeel.equals(info.getName())) {
+				            UIManager.setLookAndFeel(info.getClassName());
+				            break;
+				        }
+				    }
+				} catch (Exception e) {
+				    // If the configured look and feel is not available, the default system look and feel will remain in place.
+				}
+				
+				try {
+					PRAiSEDemoApp window = new PRAiSEDemoApp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PRAiSEDemoApp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1000, 640);
+		frame.setTitle("PRAiSE: Relational Probabilistic Inference Demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel backgroundPanel = new JPanel();
+		frame.getContentPane().add(backgroundPanel, BorderLayout.CENTER);
+		backgroundPanel.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane inputOutputSplitPane = new JSplitPane();
+		inputOutputSplitPane.setResizeWeight(0.3);
+		inputOutputSplitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		inputOutputSplitPane.setOneTouchExpandable(true);
+		backgroundPanel.add(inputOutputSplitPane, BorderLayout.CENTER);
+		
+		JPanel inputPanel = new JPanel();
+		inputPanel.setPreferredSize(new Dimension(400, 500));
+		inputOutputSplitPane.setLeftComponent(inputPanel);
+		inputPanel.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setResizeWeight(0.9);
+		inputPanel.add(splitPane, BorderLayout.CENTER);
+		
+		JPanel editorPanel = new JPanel();
+		editorPanel.setPreferredSize(new Dimension(400, 500));
+		splitPane.setLeftComponent(editorPanel);
+		
+		JPanel queryPanel = new JPanel();
+		queryPanel.setPreferredSize(new Dimension(300, 400));
+		splitPane.setRightComponent(queryPanel);
+		
+		OutputPanel outputPanel = new OutputPanel();
+		outputPanel.setPreferredSize(new Dimension(400, 60));
+		inputOutputSplitPane.setRightComponent(outputPanel);
+		
+		JPanel controlPanel = new JPanel();
+		backgroundPanel.add(controlPanel, BorderLayout.NORTH);
+		
+		JPanel notificationPanel = new JPanel();
+		backgroundPanel.add(notificationPanel, BorderLayout.SOUTH);
+	}
+
+}
