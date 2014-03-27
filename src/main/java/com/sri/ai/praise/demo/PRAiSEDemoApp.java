aeee93fc9674211e50b4c8c01eff25458600919f
ctjoreilly <ctjoreilly@e74e62f3-1872-5a1b-a7a8-c7220c51b9cb>
2014-03-27 22:49:37 +0000

    - trivial tidy up of import statements.
    git-svn-id: https://aic-praise.googlecode.com/svn/trunk@1175 e74e62f3-1872-5a1b-a7a8-c7220c51b9cb
    
    Former-commit-id: 447013dd7b4473331b188d55d74d9657bce2d8fa

--- src/main/java/com/sri/ai/praise/demo/PRAiSEDemoApp.java
+++ src/main/java/com/sri/ai/praise/demo/PRAiSEDemoApp.java
@@ -37,46 +37,36 @@
  */
 package com.sri.ai.praise.demo;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
-
-import com.google.common.annotations.Beta;
-import com.sri.ai.expresso.ExpressoConfiguration;
-import com.sri.ai.expresso.core.DefaultSymbol;
-import com.sri.ai.grinder.GrinderConfiguration;
-import com.sri.ai.praise.demo.model.Example;
-import com.sri.ai.praise.demo.model.Example1;
-import com.sri.ai.praise.demo.model.Example2;
-import com.sri.ai.praise.demo.model.Example3;
-import com.sri.ai.praise.demo.model.Example4;
-import com.sri.ai.praise.demo.model.Example5;
-import com.sri.ai.praise.demo.model.Example6;
-import com.sri.ai.praise.demo.model.Example7;
-
-import javax.swing.JPanel;
-
 import java.awt.BorderLayout;
-
-import javax.swing.JSplitPane;
-
 import java.awt.Dimension;
-
-import javax.swing.JTabbedPane;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.ChangeEvent;
-
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import com.google.common.annotations.Beta;
+import com.sri.ai.expresso.ExpressoConfiguration;
+import com.sri.ai.expresso.core.DefaultSymbol;
+import com.sri.ai.grinder.GrinderConfiguration;
+import com.sri.ai.praise.demo.model.Example;
+import com.sri.ai.praise.demo.model.Example2;
+import com.sri.ai.praise.demo.model.Example3;
+import com.sri.ai.praise.demo.model.Example7;
 
 /**
  * 
