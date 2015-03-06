dd02bf60598245cba4f941c01587a2ae30c6f1c1
ctjoreilly <ctjoreilly@e74e62f3-1872-5a1b-a7a8-c7220c51b9cb>
2015-03-06 00:12:45 +0000

    Re-factor Hugin output logic so that it can be more easily re-used when using a Markov Network generated by a UAI model.
    git-svn-id: https://aic-praise.googlecode.com/svn/trunk@1385 e74e62f3-1872-5a1b-a7a8-c7220c51b9cb
    
    Former-commit-id: 7e50edfa1798e7a94e6a406e88e4519677ce14d6

--- src/main/java/com/sri/ai/praise/model/export/ExportToHuginDotNetBayesianNetworkFormat.java
+++ src/main/java/com/sri/ai/praise/model/export/ExportToHuginDotNetBayesianNetworkFormat.java
@@ -1,3 +1,40 @@
+/*
+ * Copyright (c) 2014, SRI International
+ * All rights reserved.
+ * Licensed under the The BSD 3-Clause License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the aic-praise nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.sri.ai.praise.model.export;
 
 import java.io.BufferedWriter;
@@ -6,7 +43,11 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.google.common.annotations.Beta;
@@ -15,7 +56,6 @@ import com.sri.ai.grinder.api.RewritingProcess;
 import com.sri.ai.praise.model.Model;
 import com.sri.ai.praise.model.ModelGrounding;
 import com.sri.ai.praise.model.ModelGrounding.GroundedModelResult;
-import com.sri.ai.praise.model.grounded.bayes.ConditionalProbabilityTable;
 import com.sri.ai.praise.model.grounded.transform.XFormMarkovToBayes;
 import com.sri.ai.util.base.Pair;
 
@@ -86,84 +126,19 @@ public class ExportToHuginDotNetBayesianNetworkFormat {
 		RewritingProcess process = groundedModelResult.getRewritingProcess();
 		GroundedModelMarkovNetwork groundedMarkovNetwork = new GroundedModelMarkovNetwork(groundedModelResult);
 		
-		// Output the random variable information
+		// Collect the date required by the Hugin Output utility.
+		Map<Integer, String>       varIdxToName        = new LinkedHashMap<>();
+		Map<Integer, List<String>> varIdxToRangeValues = new LinkedHashMap<>();
+		
 		for (int i = 0; i < groundedMarkovNetwork.numberVariables(); i++) {
-			Expression rv = groundedMarkovNetwork.getRandomVariable(i);
-			StringJoiner sj = new StringJoiner("\n");
+			Expression rvExpr = groundedMarkovNetwork.getRandomVariable(i);
+			String     rvName = rvExpr.toString();
 			
-			sj.add("node "+getLegalHuginId(rv));
-			sj.add("{");
-			sj.add("  states = "+getRange(rv, process));
-			sj.add("  label  = \""+rv+"\";");
-			sj.add("}");
-			
-			output(sj.toString(), writer);
+			varIdxToName.put(i, rvName);
+			varIdxToRangeValues.put(i, Model.range(rvExpr, process).stream().map(e -> e.toString()).collect(Collectors.toList()));
 		}
 		
 		// Output the CPTs
-		XFormMarkovToBayes.transform(groundedMarkovNetwork, new XFormMarkovToBayes.BayesOutputListener() {
-			
-			@Override
-			public void newCPT(ConditionalProbabilityTable cpt) {
-				StringJoiner sj = new StringJoiner("\n");
-				sj.add("potential "+getPotentialSignature(groundedMarkovNetwork, cpt));
-				sj.add("{");
-				sj.add("    data = "+getPotentialData(cpt));
-				sj.add("}");
-				
-				output(sj.toString(), writer);
-				
-			}
-		});
-	}
-	
-	//
-	// PRIVATE
-	//
-	private static void output(String toOutput, Writer writer) {
-		// Write to the console so the user can see the output as it occurs
-		System.out.println(toOutput);
-		try {
-			writer.write(toOutput);
-			writer.write("\n");
-		}
-		catch (IOException ioe) {
-			throw new RuntimeException("Exception writing to output file", ioe);
-		}
-	}
-	
-	private static String getLegalHuginId(Expression rv) {
-		return rv.toString().replace('(', '_').replace(',', '_').replace(' ', '_').replace(')', '_');
-	}
-	
-	private static String getRange(Expression rv, RewritingProcess process) {
-		// (\"false\" \"true\");
-		StringJoiner sj = new StringJoiner(" ", "(", ");");
-		for (Expression rangeValue : Model.range(rv, process)) {
-			sj.add("\""+rangeValue.toString()+"\"");
-		}
-		return sj.toString();
-	}
-	
-	private static String getPotentialSignature(GroundedModelMarkovNetwork groundedMarkovNetwork, ConditionalProbabilityTable cpt) {
-		StringJoiner sj = new StringJoiner(" ", "(", ")");
-		
-		sj.add(getLegalHuginId(groundedMarkovNetwork.getRandomVariable(cpt.getChildVariableIndex())));
-		sj.add("|");
-		for (Integer p : cpt.getParentVariableIndexes()) {
-			sj.add(getLegalHuginId(groundedMarkovNetwork.getRandomVariable(p)));
-		}
-		
-		return sj.toString();
-	}
-	
-	private static String getPotentialData(ConditionalProbabilityTable cpt) {
-		StringJoiner sj = new StringJoiner(" ", "(", ");");
-		
-		for (Double d : cpt.getTable().getEntries()) {
-			sj.add(""+d);
-		}
-		
-		return sj.toString();
+		XFormMarkovToBayes.transform(groundedMarkovNetwork, new HuginOutput(writer, varIdxToName, varIdxToRangeValues));
 	}
 }
