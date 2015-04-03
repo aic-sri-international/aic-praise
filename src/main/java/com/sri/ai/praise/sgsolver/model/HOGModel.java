fc8a2ab35bce193386b5ede1137f01b1b9c146f7
ctjoreilly <ctjoreilly@e74e62f3-1872-5a1b-a7a8-c7220c51b9cb>
2015-04-03 00:15:42 +0000

    Add additional semantic validation logic related to HOGM parsing.
    git-svn-id: https://aic-praise.googlecode.com/svn/trunk@1446 e74e62f3-1872-5a1b-a7a8-c7220c51b9cb
    
    Former-commit-id: ec6731735c95b8778565ce08247685c3467cbba2

--- src/main/java/com/sri/ai/praise/sgsolver/model/HOGModel.java
+++ src/main/java/com/sri/ai/praise/sgsolver/model/HOGModel.java
@@ -39,6 +39,7 @@ package com.sri.ai.praise.sgsolver.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -47,10 +48,15 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.google.common.annotations.Beta;
+import com.google.common.base.Predicate;
 import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.helper.Expressions;
+import com.sri.ai.expresso.helper.SubExpressionsDepthFirstIterator;
+import com.sri.ai.grinder.core.PrologConstantPredicate;
 import com.sri.ai.grinder.library.FunctorConstants;
 import com.sri.ai.grinder.library.boole.ForAll;
 import com.sri.ai.grinder.library.boole.ThereExists;
+import com.sri.ai.grinder.library.controlflow.IfThenElse;
 import com.sri.ai.grinder.library.set.tuple.Tuple;
 import com.sri.ai.praise.model.RandomVariableDeclaration;
 import com.sri.ai.praise.model.SortDeclaration;
@@ -90,13 +96,89 @@ public class HOGModel {
 		
 		KNOWN_NUMERIC_FUNCTORS = Collections.unmodifiableSet(knownNumericFunctors);
 	}
+	
+	public final static Set<String> KNOWN_UNDETERMINED_FUNCTORS;
+	static {
+		Set<String> undetermined = new LinkedHashSet<>();
+		undetermined.add(FunctorConstants.IF_THEN_ELSE);
+		
+		KNOWN_UNDETERMINED_FUNCTORS = Collections.unmodifiableSet(undetermined);
+	}
+	
 	public final static Set<String> KNOWN_FUNCTORS;
 	static {
 		Set<String> knownFunctors = new LinkedHashSet<>();
 		knownFunctors.addAll(KNOWN_BOOLEAN_FUNCTORS);
 		knownFunctors.addAll(KNOWN_NUMERIC_FUNCTORS);
+		knownFunctors.addAll(KNOWN_UNDETERMINED_FUNCTORS);
 		
 		KNOWN_FUNCTORS = Collections.unmodifiableSet(knownFunctors);
+	}
+	
+	public final static Set<String> KNOWN_ARITY_1_FUNCTORS;
+	static {
+		Set<String> arity1 = new LinkedHashSet<>();
+		arity1.add(FunctorConstants.NOT);
+		
+		KNOWN_ARITY_1_FUNCTORS = Collections.unmodifiableSet(arity1);
+	}
+	
+	public final static Set<String> KNOWN_ARITY_2_FUNCTORS;
+	static {
+		Set<String> arity2 = new LinkedHashSet<>();
+		arity2.add(ForAll.LABEL);
+		arity2.add(ThereExists.LABEL);
+		arity2.add(FunctorConstants.IMPLICATION);
+		arity2.add(FunctorConstants.EQUIVALENCE);
+		arity2.add(FunctorConstants.LESS_THAN);
+		arity2.add(FunctorConstants.LESS_THAN_OR_EQUAL_TO);
+		arity2.add(FunctorConstants.DISEQUALITY);
+		arity2.add(FunctorConstants.GREATER_THAN_OR_EQUAL_TO);
+		arity2.add(FunctorConstants.GREATER_THAN);
+		arity2.add(FunctorConstants.EXPONENTIATION);
+		arity2.add(FunctorConstants.DIVISION);
+		
+		KNOWN_ARITY_2_FUNCTORS = Collections.unmodifiableSet(arity2);
+	}
+	
+	public final static Set<String> KNOWN_ARITY_3_FUNCTORS;
+	static {
+		Set<String> arity3 = new LinkedHashSet<>();
+		arity3.add(FunctorConstants.IF_THEN_ELSE);
+		
+		KNOWN_ARITY_3_FUNCTORS = Collections.unmodifiableSet(arity3);
+	}
+	
+	public final static Set<String> KNOWN_ARITY_1_OR_2_FUNCTORS;
+	static {
+		Set<String> arity1or2 = new LinkedHashSet<>();
+		// i.e. unary minus or normal subtraction
+		arity1or2.add(FunctorConstants.MINUS); 
+		KNOWN_ARITY_1_OR_2_FUNCTORS = Collections.unmodifiableSet(arity1or2);
+	}
+	
+	public final static Set<String> KNOWN_ARITY_0_OR_MORE_FUNCTORS;
+	static {
+		Set<String> arity0Plus = new LinkedHashSet<>();
+		arity0Plus.add(FunctorConstants.AND);
+		arity0Plus.add(FunctorConstants.OR);
+		arity0Plus.add(FunctorConstants.EQUALITY);	
+		
+		KNOWN_ARITY_0_OR_MORE_FUNCTORS = Collections.unmodifiableSet(arity0Plus);
+	}
+	
+	public final static Set<String> KNOWN_ARITY_GREATER_THAN_1_FUNCTORS;
+	static {
+		Set<String> arity2Plus = new LinkedHashSet<>();
+		
+		arity2Plus.add(FunctorConstants.TIMES);
+		arity2Plus.add(FunctorConstants.PLUS);		
+		
+		KNOWN_ARITY_GREATER_THAN_1_FUNCTORS = Collections.unmodifiableSet(arity2Plus);
+	}
+	
+	public enum TermCategoryType {
+		BOOLEAN, NUMERIC, OTHER, INVALID
 	}
 
 	public static Expression validateAndConstruct(List<StatementInfo> sortDecs, List<StatementInfo> randomVarDecs, List<StatementInfo> terms) {
@@ -121,6 +203,12 @@ public class HOGModel {
 		//
 		List<HOGModelError> errors = new ArrayList<>();
 		//
+		Set<String> booleanTypeFunctors = new LinkedHashSet<>(KNOWN_BOOLEAN_FUNCTORS);
+		Set<String> numericTypeFunctors = new LinkedHashSet<>(KNOWN_NUMERIC_FUNCTORS);
+		Set<String> otherTypeFunctors   = new LinkedHashSet<>();
+		//
+		Predicate<Expression> isPrologConstant = new PrologConstantPredicate();
+		//
 		Set<Expression> sortConstants = new LinkedHashSet<>();
 		Map<Expression, SortDeclaration> sorts = new LinkedHashMap<>();
 		Map<Expression, RandomVariableDeclaration> randoms = new LinkedHashMap<>();
@@ -128,13 +216,18 @@ public class HOGModel {
 		HOGMModelValidator(List<StatementInfo> sortStatements, List<StatementInfo> randomVariableStatements, List<StatementInfo> termStatements) {
 			validateSortStatements(sortStatements);
 			validateRandomVariableStatements(randomVariableStatements);
-
-// TODO -  validate
-			termStatements.forEach(t -> conditionedPotentials.add(t.statement));	
+			validateTermStatements(termStatements);	
 			
 			// Map validated values
 			sortDeclarations.addAll(sorts.values().stream().map(sd -> sd.getSortDeclaration()).collect(Collectors.toList()));
 			randomVariableDeclarations.addAll(randoms.values().stream().map(rd -> rd.getRandomVariableDeclaration()).collect(Collectors.toList()));
+			conditionedPotentials.addAll(termStatements.stream().map(termStatement -> {
+				Expression result = termStatement.statement;
+				if (!IfThenElse.isIfThenElse(termStatement.statement)) {
+					result =  IfThenElse.make(termStatement.statement, Expressions.ONE, Expressions.ZERO);
+				}
+				return result;
+			}).collect(Collectors.toList()));
 		}
 		
 		boolean isValid() {
@@ -193,9 +286,169 @@ public class HOGModel {
 						if (KNOWN_FUNCTORS.contains(rvDeclaration.getName().toString())) {
 							newError(Type.RANDOM_VARIABLE_NAME_SAME_AS_IN_BUILT_FUNCTOR, rvDeclaration.getName(), rvStatement);
 						}
+						// Track the category type of the random variable
+						if (rvDeclaration.getRangeSort().equals(SortDeclaration.IN_BUILT_BOOLEAN.getName())) {
+							booleanTypeFunctors.add(functorName(rvDeclaration.getName()));
+						}
+						else if (rvDeclaration.getRangeSort().equals(SortDeclaration.IN_BUILT_NUMBER.getName())) {
+							numericTypeFunctors.add(functorName(rvDeclaration.getName()));
+						}
+						else {
+							otherTypeFunctors.add(functorName(rvDeclaration.getName()));
+						}
 					}
 				}
 			});
+		}
+		
+		void validateTermStatements(List<StatementInfo> termStatements) {
+			termStatements.forEach(termStatement -> {
+				// Ensure all functors are known and have correct arity
+				validateFunctors(termStatement);
+				
+				Expression statement = termStatement.statement;
+				// Determine type
+				TermCategoryType termType = determineTermCategoryType(statement);
+				if (IfThenElse.isIfThenElse(statement)) {
+					if (termType != TermCategoryType.NUMERIC) {
+						newError(Type.TERM_CONDITONAL_STATEMENT_MUST_BE_OF_TYPE_NUMERIC, "", termStatement);
+					}
+				}
+				else if (termType != TermCategoryType.BOOLEAN) {
+					newError(Type.TERM_NON_CONDITIONAL_STATEMENT_MUST_BE_OF_TYPE_BOOLEAN, "", termStatement);
+				}	
+			});
+		}
+		
+		TermCategoryType determineTermCategoryType(Expression expr) {
+			TermCategoryType result = TermCategoryType.INVALID;
+			
+			if (IfThenElse.isIfThenElse(expr)) {
+				TermCategoryType condition  = determineTermCategoryType(IfThenElse.condition(expr));
+				TermCategoryType thenBranch = determineTermCategoryType(IfThenElse.thenBranch(expr));
+				TermCategoryType elseBranch = determineTermCategoryType(IfThenElse.elseBranch(expr));
+				// Ensure legal 
+				if (condition == TermCategoryType.BOOLEAN && thenBranch == elseBranch) {
+					result = thenBranch; // same as elseBranch
+				}
+			}
+			else {
+				Expression functor = expr.getFunctor();
+				if (functor == null) {
+					if (Expressions.FALSE.equals(expr) || Expressions.TRUE.equals(expr)) {
+						result = TermCategoryType.BOOLEAN;
+					}
+					else if (Expressions.isNumber(expr)) {
+						result = TermCategoryType.NUMERIC;
+					}
+					else if (isPrologConstant.apply(expr)) {
+						result = TermCategoryType.OTHER;
+					}
+					else {
+						result = TermCategoryType.OTHER;
+					}
+				}
+				else {
+					String functorName = functorName(functor);
+					if (booleanTypeFunctors.contains(functorName)) {
+						result = TermCategoryType.BOOLEAN;
+					}
+					else if (numericTypeFunctors.contains(functorName)) {
+						result = TermCategoryType.NUMERIC;
+					}
+					else if (otherTypeFunctors.contains(functorName)) {
+						result = TermCategoryType.OTHER;
+					}
+				}
+			}
+			
+			return result;
+		}
+		
+		void validateFunctors(StatementInfo termStatement) {
+			Iterator<Expression> subExpressionsIterator =  new SubExpressionsDepthFirstIterator(termStatement.statement);
+		
+			subExpressionsIterator.forEachRemaining(expr -> {
+				if (expr.getFunctor() != null) {
+					if (isKnownFunctor(expr.getFunctor())) {
+						if (!isValidKnownFunctorArity(expr)) {
+							newError(Type.TERM_ARITY_OF_FUNCTOR_DOES_NOT_MATCH_DECLARATION, expr, termStatement);
+						}
+// TODO - validate arguments are of correct types						
+					}
+					else if (isDeclaredRandomFunctor(expr.getFunctor())) {
+						if (!isValidDeclaredRandomFunctorArity(expr)) {
+							newError(Type.TERM_ARITY_OF_FUNCTOR_DOES_NOT_MATCH_DECLARATION, expr, termStatement);
+						}
+// TODO - validate arguments are of correct type						
+					}
+					else {					
+						newError(Type.TERM_TYPE_OF_FUNCTOR_NOT_DECLARED, expr.getFunctor(), termStatement);
+					}
+				}
+			});
+		}
+		
+		boolean isKnownFunctor(Expression functorName) {
+			boolean result = false;
+			if (KNOWN_FUNCTORS.contains(functorName(functorName))) {
+				result = true;
+			}
+			return result;
+		}
+		
+		boolean isDeclaredRandomFunctor(Expression functorName) {
+			boolean result = false;
+			if (randoms.containsKey(functorName)) {
+				result = true;
+			}
+			return result;
+		}
+		
+		boolean isValidKnownFunctorArity(Expression expr) {
+			boolean result = true;
+			String functorName = functorName(expr.getFunctor());
+
+			if (KNOWN_ARITY_1_FUNCTORS.contains(functorName)
+					&& expr.numberOfArguments() != 1) {
+				result = false;
+			}
+			else if (KNOWN_ARITY_2_FUNCTORS.contains(functorName)
+					&& expr.numberOfArguments() != 2) {
+				result = false;
+			}
+			else if (KNOWN_ARITY_3_FUNCTORS.contains(functorName)
+					&& expr.numberOfArguments() != 3) {
+				result = false;
+			}
+			else if (KNOWN_ARITY_1_OR_2_FUNCTORS.contains(functorName)
+					&& !(expr.numberOfArguments() == 1 || expr.numberOfArguments() == 2)) {
+				result = false;
+			}
+			else if (KNOWN_ARITY_0_OR_MORE_FUNCTORS.contains(functorName) 
+					&& expr.numberOfArguments() < 0) { // Should never happen
+				result = false;
+			}
+			else if (KNOWN_ARITY_GREATER_THAN_1_FUNCTORS.contains(functorName) 
+					&& expr.numberOfArguments() < 2) {
+				result = false;
+			}
+
+			return result;
+		}
+		
+		boolean isValidDeclaredRandomFunctorArity(Expression expr) {
+			boolean result = true;
+			RandomVariableDeclaration rvDeclaration = randoms.get(expr.getFunctor());
+			if (rvDeclaration.getArity().intValue() != expr.numberOfArguments()) {
+				result = false;
+			}
+			return result;
+		}
+		
+		String functorName(Expression exprFunctorName) {
+			String result = exprFunctorName.getValue().toString();
+			return result;
 		}
 		
 		void newError(Type errorType, Object msg, StatementInfo statement) {
