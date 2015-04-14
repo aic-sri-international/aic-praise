b2f6c1f0b90f8eccb1a49fd38273f79f6d6f1f52
ctjoreilly <ctjoreilly@e74e62f3-1872-5a1b-a7a8-c7220c51b9cb>
2015-04-14 17:30:47 +0000

    Trivial imports tidy up.
    git-svn-id: https://aic-praise.googlecode.com/svn/trunk@1485 e74e62f3-1872-5a1b-a7a8-c7220c51b9cb
    
    Former-commit-id: 21aed1eddf753b94c31ed9bd6a1758e56a386adc

--- src/main/java/com/sri/ai/praise/sgsolver/hogm/antlr/HOGModelVisitor.java
+++ src/main/java/com/sri/ai/praise/sgsolver/hogm/antlr/HOGModelVisitor.java
@@ -41,7 +41,6 @@ import java.util.ArrayList;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.NotNull;
