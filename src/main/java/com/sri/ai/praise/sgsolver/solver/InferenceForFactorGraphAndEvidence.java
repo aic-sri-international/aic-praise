8534f2a70eb68a63dddb137ee645abc3705ad71e
Rodrigo de Salvo Braz <braz@ai.sri.com>
2015-11-03 20:25:04 -0800

    - reflecting change of EqualityConstraintTheory constructor.

--- src/main/java/com/sri/ai/praise/sgsolver/solver/InferenceForFactorGraphAndEvidence.java
+++ src/main/java/com/sri/ai/praise/sgsolver/solver/InferenceForFactorGraphAndEvidence.java
@@ -166,7 +166,7 @@ public class InferenceForFactorGraphAndEvidence {
 		// The solver for the parameters above.
 		com.sri.ai.grinder.sgdpll2.api.ConstraintTheory sgdpll2ConstraintTheory =
 				new CompoundConstraintTheory(
-				new com.sri.ai.grinder.sgdpll2.theory.equality.EqualityConstraintTheory(),
+				new com.sri.ai.grinder.sgdpll2.theory.equality.EqualityConstraintTheory(true),
 				new com.sri.ai.grinder.sgdpll2.theory.propositional.PropositionalConstraintTheory());
 		SymbolicCommonInterpreterWithLiteralConditioning simplifier = new SymbolicCommonInterpreterWithLiteralConditioning(sgdpll2ConstraintTheory, true);
 		if (useFactorization) {
