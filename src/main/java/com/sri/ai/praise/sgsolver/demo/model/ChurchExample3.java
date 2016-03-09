d29ab29aa0732e38d2c6e64260bdd077ceb385b2
Ciaran O'Reilly <oreilly@ashland>
2016-03-08 16:48:14 -0800

    Some tidy up.

--- src/main/java/com/sri/ai/praise/sgsolver/demo/model/ChurchExample3.java
+++
@@ -1,7 +0,0 @@
-package com.sri.ai.praise.sgsolver.demo.model;
-
-public class ChurchExample3 extends ExamplePages {
-	public ChurchExample3() {
-		super("Church Example 3 Program", getExamplePagesFromResource("churcheg3.praise"));
-	}
-}
--- src/main/java/com/sri/ai/praise/sgsolver/demo/model/ChurchExample4.java
+++
@@ -1,7 +0,0 @@
-package com.sri.ai.praise.sgsolver.demo.model;
-
-public class ChurchExample4 extends ExamplePages {
-	public ChurchExample4() {
-		super("Church Example 4 Program", getExamplePagesFromResource("churcheg4.praise"));
-	}
-}
