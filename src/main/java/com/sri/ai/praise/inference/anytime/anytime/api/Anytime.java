bc43f795c5ce5c4cea71096b496200718b0bd129
Gabriel Azevedo Ferreira <gabriel.azevedo-ferreira@polytechnique.edu>
2018-03-06 11:57:45 +0800

    typo

--- src/main/java/com/sri/ai/praise/inference/anytime/anytime/api/Anytime.java
+++ src/main/java/com/sri/ai/praise/inference/anytime/anytime/api/Anytime.java
@@ -50,7 +50,7 @@ import com.sri.ai.util.base.NullaryFunction;
  * Ideally, the values of the iterator are better and better approximations,
  * hopefully converging to the exact value although that is not required at this level.
  * <p>
- * The current value of the iterator is avaiable at {@link #getCurrentApproximation()},
+ * The current value of the iterator is available at {@link #getCurrentApproximation()},
  * which does not iterate to the next value.
  * <p>
  * It extends the interface of nullary function of an approximation.
