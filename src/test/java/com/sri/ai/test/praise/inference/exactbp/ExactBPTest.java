8ef51d6882d85b8fb069e37f6c1de8e218a2bde4
Gabriel Azevedo Ferreira <gabriel.azevedo-ferreira@polytechnique.edu>
2018-03-13 08:19:56 +0800

    adding grid test

--- src/test/java/com/sri/ai/test/praise/inference/exactbp/ExactBPTest.java
+++ src/test/java/com/sri/ai/test/praise/inference/exactbp/ExactBPTest.java
@@ -149,8 +149,55 @@ public class ExactBPTest {
 		queryVariableString = "I";
 		expected = parse("if I = 2 then 0.1875 else if I = 3 then 0.1875 else 0"); // Note: ExactBP returns an arbitrary unnormalized message
 		runTest(variableAndTypes, factorNetworkString, queryVariableString, expected);
-	}
+		
 
+		//
+		// A00 -- A01 -- ... -- A04
+		//  |      |             |
+		// A10 -- A11 -- ... -- A14
+		//  .	   .    .        .
+		//  .	   .      .      .
+		//  .	   .        .    .
+		//  |      |          .  |
+		// A40 -- A41 -- ... -- A44
+		//
+		variableAndTypes = new String[]{"A_0_0", "Boolean","A_1_0", "Boolean","A_2_0", "Boolean","A_3_0", "Boolean","A_4_0", "Boolean",
+										"A_0_1", "Boolean","A_1_1", "Boolean","A_2_1", "Boolean","A_3_1", "Boolean","A_4_1", "Boolean",
+										"A_0_2", "Boolean","A_1_2", "Boolean","A_2_2", "Boolean","A_3_2", "Boolean","A_4_2", "Boolean",
+										"A_0_3", "Boolean","A_1_3", "Boolean","A_2_3", "Boolean","A_3_3", "Boolean","A_4_3", "Boolean",
+										"A_0_4", "Boolean","A_1_4", "Boolean","A_2_4", "Boolean","A_3_4", "Boolean","A_4_4", "Boolean"};
+		factorNetworkString = "(" 
+				+ "if A_1_1 = false then if A_1_2 = false then 0 else if A_1_2 = true then 3 else 0 else if A_1_1 = true then if A_1_2 = false then 2 else if A_1_2 = true then 3 else 0 else 0, "
+				+ "if A_1_1 = false then if A_2_1 = false then 3 else if A_2_1 = true then 4 else 0 else if A_1_1 = true then if A_2_1 = false then 7 else if A_2_1 = true then 8 else 0 else 0, "
+				+ "if A_2_1 = false then if A_2_2 = false then 3 else if A_2_2 = true then 8 else 0 else if A_2_1 = true then if A_2_2 = false then 8 else if A_2_2 = true then 7 else 0 else 0, "
+				+ "if A_1_3 = false then if A_2_3 = false then 4 else if A_2_3 = true then 6 else 0 else if A_1_3 = true then if A_2_3 = false then 7 else if A_2_3 = true then 4 else 0 else 0, "
+				+ "if A_3_2 = false then if A_3_3 = false then 1 else if A_3_3 = true then 4 else 0 else if A_3_2 = true then if A_3_3 = false then 5 else if A_3_3 = true then 1 else 0 else 0, "
+				+ "if A_1_0 = false then if A_2_0 = false then 3 else if A_2_0 = true then 3 else 0 else if A_1_0 = true then if A_2_0 = false then 4 else if A_2_0 = true then 8 else 0 else 0, "
+				+ "if A_2_0 = false then if A_3_0 = false then 3 else if A_3_0 = true then 0 else 0 else if A_2_0 = true then if A_3_0 = false then 0 else if A_3_0 = true then 8 else 0 else 0, "
+				+ "if A_3_0 = false then if A_3_1 = false then 5 else if A_3_1 = true then 4 else 0 else if A_3_0 = true then if A_3_1 = false then 6 else if A_3_1 = true then 1 else 0 else 0, "
+				+ "if A_0_0 = false then if A_0_1 = false then 7 else if A_0_1 = true then 3 else 0 else if A_0_0 = true then if A_0_1 = false then 4 else if A_0_1 = true then 2 else 0 else 0, "
+				+ "if A_1_0 = false then if A_1_1 = false then 7 else if A_1_1 = true then 8 else 0 else if A_1_0 = true then if A_1_1 = false then 6 else if A_1_1 = true then 7 else 0 else 0, "
+				+ "if A_2_2 = false then if A_3_2 = false then 5 else if A_3_2 = true then 8 else 0 else if A_2_2 = true then if A_3_2 = false then 5 else if A_3_2 = true then 5 else 0 else 0, "
+				+ "if A_0_2 = false then if A_0_3 = false then 5 else if A_0_3 = true then 3 else 0 else if A_0_2 = true then if A_0_3 = false then 7 else if A_0_3 = true then 0 else 0 else 0, "
+				+ "if A_0_2 = false then if A_1_2 = false then 2 else if A_1_2 = true then 4 else 0 else if A_0_2 = true then if A_1_2 = false then 2 else if A_1_2 = true then 0 else 0 else 0, "
+				+ "if A_2_3 = false then if A_3_3 = false then 7 else if A_3_3 = true then 5 else 0 else if A_2_3 = true then if A_3_3 = false then 7 else if A_3_3 = true then 7 else 0 else 0, "
+				+ "if A_2_1 = false then if A_3_1 = false then 7 else if A_3_1 = true then 4 else 0 else if A_2_1 = true then if A_3_1 = false then 3 else if A_3_1 = true then 3 else 0 else 0, "
+				+ "if A_3_1 = false then if A_3_2 = false then 0 else if A_3_2 = true then 0 else 0 else if A_3_1 = true then if A_3_2 = false then 4 else if A_3_2 = true then 3 else 0 else 0, "
+				+ "if A_0_1 = false then if A_0_2 = false then 6 else if A_0_2 = true then 5 else 0 else if A_0_1 = true then if A_0_2 = false then 0 else if A_0_2 = true then 6 else 0 else 0, "
+				+ "if A_0_1 = false then if A_1_1 = false then 5 else if A_1_1 = true then 5 else 0 else if A_0_1 = true then if A_1_1 = false then 3 else if A_1_1 = true then 2 else 0 else 0, "
+				+ "if A_1_2 = false then if A_1_3 = false then 7 else if A_1_3 = true then 3 else 0 else if A_1_2 = true then if A_1_3 = false then 2 else if A_1_3 = true then 2 else 0 else 0, "
+				+ "if A_1_2 = false then if A_2_2 = false then 7 else if A_2_2 = true then 5 else 0 else if A_1_2 = true then if A_2_2 = false then 6 else if A_2_2 = true then 4 else 0 else 0, "
+				+ "if A_2_0 = false then if A_2_1 = false then 0 else if A_2_1 = true then 5 else 0 else if A_2_0 = true then if A_2_1 = false then 8 else if A_2_1 = true then 6 else 0 else 0, "
+				+ "if A_0_0 = false then if A_1_0 = false then 5 else if A_1_0 = true then 6 else 0 else if A_0_0 = true then if A_1_0 = false then 4 else if A_1_0 = true then 3 else 0 else 0, "
+				+ "if A_0_3 = false then if A_1_3 = false then 1 else if A_1_3 = true then 3 else 0 else if A_0_3 = true then if A_1_3 = false then 2 else if A_1_3 = true then 1 else 0 else 0, "
+				+ "if A_2_2 = false then if A_2_3 = false then 6 else if A_2_3 = true then 1 else 0 else if A_2_2 = true then if A_2_3 = false then 3 else if A_2_3 = true then 3 else 0 else 0"
+				+ ")";
+		
+		queryVariableString = "A_0_0";
+		expected = parse("if not A_0_0 then 2515404149056770048 else 857920100616142848"); // Note: ExactBP returns an arbitrary unnormalized message
+		runTest(variableAndTypes, factorNetworkString, queryVariableString, expected);
+	}
+	
 	private void runTest(String[] variableAndTypes, String factorNetworkString, String queryVariableString, Expression expected) {
 		Context context = new TrueContext(new CommonTheory()).extendWithSymbolsAndTypes(variableAndTypes);
 		ExpressionFactorNetwork factorNetwork = new ExpressionFactorNetwork(factorNetworkString, context);
