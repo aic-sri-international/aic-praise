64530d2bc1cd0d93391c70ffe6075fa9be721836
Rodrigo de Salvo Braz <braz@ai.sri.com>
2019-01-11 13:59:34 -0200

    - made DefaultVariable equality depend on inner object.

--- src/main/java/com/sri/ai/praise/core/representation/interfacebased/factor/core/base/DefaultVariable.java
+++ src/main/java/com/sri/ai/praise/core/representation/interfacebased/factor/core/base/DefaultVariable.java
@@ -23,30 +23,30 @@ public class DefaultVariable implements Variable {
 		throw new Error("getValues not supported by " + getClass());
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((object == null) ? 0 : object.hashCode());
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		DefaultVariable other = (DefaultVariable) obj;
-//		if (object == null) {
-//			if (other.object != null)
-//				return false;
-//		} else if (!object.equals(other.object))
-//			return false;
-//		return true;
-//	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((object == null) ? 0 : object.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DefaultVariable other = (DefaultVariable) obj;
+		if (object == null) {
+			if (other.object != null)
+				return false;
+		} else if (!object.equals(other.object))
+			return false;
+		return true;
+	}
 	
 	public Object getObject() {
 		return object;
