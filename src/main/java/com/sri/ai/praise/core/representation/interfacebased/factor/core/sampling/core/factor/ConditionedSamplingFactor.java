28b7361d2e2eb46b8081d6179070b1865401c39e
Rodrigo de Salvo Braz <braz@ai.sri.com>
2019-01-26 04:30:15 -0200

    - added sampling rules optimization to ConditionedSamplingFactor

--- src/main/java/com/sri/ai/praise/core/representation/interfacebased/factor/core/sampling/core/factor/ConditionedSamplingFactor.java
+++ src/main/java/com/sri/ai/praise/core/representation/interfacebased/factor/core/sampling/core/factor/ConditionedSamplingFactor.java
@@ -1,7 +1,9 @@
 package com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.core.factor;
 
+import static com.sri.ai.util.Util.arrayList;
 import static com.sri.ai.util.Util.collectToArrayList;
 import static com.sri.ai.util.Util.makeProxy;
+import static java.util.stream.Collectors.toCollection;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -13,6 +15,11 @@ import java.util.Map.Entry;
 import com.sri.ai.praise.core.representation.interfacebased.factor.api.Variable;
 import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.api.factor.SamplingFactor;
 import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.api.sample.Sample;
+import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.api.schedule.SamplingGoal;
+import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.api.schedule.SamplingRuleSet;
+import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.core.schedule.DefaultSamplingRuleSet;
+import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.core.schedule.SamplingRule;
+import com.sri.ai.praise.core.representation.interfacebased.factor.core.sampling.core.schedule.goal.VariableIsDefinedGoal;
 
 public interface ConditionedSamplingFactor extends SamplingFactor {
 
@@ -29,13 +36,21 @@ public interface ConditionedSamplingFactor extends SamplingFactor {
 
 		private SamplingFactor samplingFactor;
 		private Sample conditioningSample;
-		private ArrayList<Variable> variables;
+		private ArrayList<Variable> conditionedVariables;
+		private SamplingRuleSet conditionedSamplingRuleSet;
+		public ConditionedSamplingFactor proxy;
 
 		public ConditionedSamplingFactorProxyInvocationHandler(SamplingFactor samplingFactor, Sample conditioningSample) {
 			this.samplingFactor = samplingFactor;
 			this.conditioningSample = conditioningSample;
+			this.conditionedVariables = makeConditionedVariables(samplingFactor, conditioningSample);
+			this.conditionedSamplingRuleSet = makeConditionedSamplingRuleSet();
+		}
+
+		private ArrayList<Variable> makeConditionedVariables(SamplingFactor samplingFactor, Sample conditioningSample) {
 			Collection<? extends Variable> variablesInAssignment = conditioningSample.getAssignment().mapValue().keySet();
-			this.variables = collectToArrayList(samplingFactor.getVariables(), v -> ! variablesInAssignment.contains(v));
+			ArrayList<Variable> conditionedVariables = collectToArrayList(samplingFactor.getVariables(), v -> ! variablesInAssignment.contains(v));
+			return conditionedVariables;
 		}
 
 		@Override
@@ -45,7 +60,10 @@ public interface ConditionedSamplingFactor extends SamplingFactor {
 				return null;
 			}
 			else if (method.getName().equals("getVariables")) {
-				return Collections.unmodifiableList(variables);
+				return Collections.unmodifiableList(conditionedVariables);
+			}
+			else if (method.getName().equals("getSamplingRuleSet")) {
+				return conditionedSamplingRuleSet;
 			}
 			else if (method.getDeclaringClass().isAssignableFrom(SamplingFactor.class)) {
 				Object result = method.invoke(samplingFactor, args);
@@ -62,5 +80,42 @@ public interface ConditionedSamplingFactor extends SamplingFactor {
 			}
 			samplingFactor.sampleOrWeigh(sample);
 		}
+		
+		private SamplingRuleSet makeConditionedSamplingRuleSet() {
+			ArrayList<SamplingRule> rules = 
+					getOriginalSamplingRules().stream()
+					.map(this::conditionOrNull)
+					.filter(c -> c != null)
+					.collect(toCollection(() -> arrayList()));
+			return new DefaultSamplingRuleSet(rules);
+		}
+
+		private ArrayList<? extends SamplingRule> getOriginalSamplingRules() {
+			return samplingFactor.getSamplingRuleSet().getSamplingRules();
+		}
+		
+		private SamplingRule conditionOrNull(SamplingRule rule) {
+			ArrayList<SamplingGoal> conditionedConsequents = getConditionedConsequents(rule);
+			if (conditionedConsequents.isEmpty()) {
+				return null;
+			}
+			else {
+				ArrayList<SamplingGoal> conditionedAntecedents = getConditionedAntecedents(rule);
+				return new SamplingRule(proxy, conditionedConsequents, conditionedAntecedents, rule.getEstimatedSuccessWeight());
+			}
+		}
+
+		private ArrayList<SamplingGoal> getConditionedConsequents(SamplingRule rule) {
+			return collectToArrayList(rule.getConsequents(), g -> ! conditioningSample.instantiates(getVariable(g)));
+		}
+
+		private ArrayList<SamplingGoal> getConditionedAntecedents(SamplingRule rule) {
+			return collectToArrayList(rule.getAntecendents(), g -> ! g.isSatisfied(conditioningSample));
+		}
+
+		private Variable getVariable(SamplingGoal variableIsDefinedGoal) {
+			return ((VariableIsDefinedGoal) variableIsDefinedGoal).getVariable();
+		}
+
 	}
 }
