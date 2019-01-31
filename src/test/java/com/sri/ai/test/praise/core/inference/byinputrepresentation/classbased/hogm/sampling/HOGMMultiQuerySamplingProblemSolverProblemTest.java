1b422b26c1cc8049b91d77fccfb5d4ba553d76e4
ddunkley <david.dunkley@sri.com>
2019-01-30 21:57:15 -0800

    Using the same function but using a different xmVariable generates the same results

--- src/test/java/com/sri/ai/test/praise/core/inference/byinputrepresentation/classbased/hogm/sampling/HOGMMultiQuerySamplingProblemSolverProblemTest.java
+++ src/test/java/com/sri/ai/test/praise/core/inference/byinputrepresentation/classbased/hogm/sampling/HOGMMultiQuerySamplingProblemSolverProblemTest.java
@@ -4,7 +4,15 @@ import static com.sri.ai.util.Util.getFirst;
 import static com.sri.ai.util.Util.list;
 import static org.junit.Assert.assertEquals;
 
+import com.sri.ai.praise.core.inference.byinputrepresentation.classbased.hogm.sampling.HOGMMultiQuerySamplingProblemSolver;
+import com.sri.ai.praise.core.representation.interfacebased.factor.core.expressionsampling.ExpressionWithProbabilityFunction;
+import com.sri.ai.util.function.api.functions.Function;
+import com.sri.ai.util.function.core.values.SetOfRealValues;
+import com.sri.ai.util.function.core.variables.RealVariable;
+import com.sri.ai.util.graph2d.api.GraphPlot;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -25,8 +33,65 @@ import com.sri.ai.util.function.core.variables.DefaultSetOfVariables;
 import com.sri.ai.util.graph2d.api.GraphSetMaker;
 
 public class HOGMMultiQuerySamplingProblemSolverProblemTest {
-
   @Test
+  public void testRangeChangeXm() {
+    String model = "random x : [-10;10]; x = Normal(0.0, 15.0);";
+    String query = "x";
+
+    HOGMMultiQuerySamplingProblemSolver queryRunner =
+        new HOGMMultiQuerySamplingProblemSolver(
+            model,
+            Collections.singletonList(query),
+            v -> 25,
+            1000,
+            new Random());
+
+    List<? extends HOGMProblemResult> hogmProblemResults = queryRunner.getResults();
+    final HOGMProblemResult hpResult = hogmProblemResults.get(0);
+    if (!hpResult.getErrors().isEmpty()) {
+      throw new RuntimeException("ERROR: " + hpResult.getErrors().get(0));
+    }
+    ExpressionWithProbabilityFunction expressionWithProbabilityFunction =
+        ((ExpressionWithProbabilityFunction)  hpResult.getResult());
+    Function function = expressionWithProbabilityFunction.getDiscretizedConditionalProbabilityDistributionFunction();
+
+    SetOfVariables inputVariables = function.getSetOfInputVariables();
+    int queryIndex =
+        Util.getIndexOfFirstSatisfyingPredicateOrMinusOne(
+            inputVariables.getVariables(), v -> v.getName().equals(query));
+
+    ArrayList<? extends Variable> nonXAxisVariables = inputVariables.getVariables();
+    nonXAxisVariables.remove(queryIndex);
+    SetOfVariables setOfNonXAxisVariables = new DefaultSetOfVariables(nonXAxisVariables);
+
+    List<? extends Value> values =
+        Util.mapIntoList(nonXAxisVariables, v -> v.getSetOfValuesOrNull().get(0));
+    DefaultAssignment assignmentOnNonXAxisVariables = new DefaultAssignment(setOfNonXAxisVariables, values);
+
+    Functions functions = Functions.functions(function);
+    Variable xmVariable = functions.getAllInputVariables().getVariables().get(queryIndex);
+
+    GraphSetMaker graphSetMaker = GraphSetMaker.graphSetMaker();
+    graphSetMaker.setFunctions(functions);
+    GraphPlot graphPlot = graphSetMaker.plot(assignmentOnNonXAxisVariables, xmVariable);
+    System.out.println(graphPlot);
+
+    // Using the same function but using a different xmVariable generates the same results ??
+
+    // use modified xm
+    SetOfRealValues origSetOfRealValues =  (SetOfRealValues) xmVariable.getSetOfValuesOrNull();
+
+    SetOfRealValues newSetOfRealValues = new SetOfRealValues(1, new BigDecimal(1), 5);
+    RealVariable newXm = new RealVariable(xmVariable.getName(), xmVariable.getUnit(), newSetOfRealValues);
+
+    graphSetMaker = GraphSetMaker.graphSetMaker();
+    graphSetMaker.setFunctions(functions);
+    graphPlot = graphSetMaker.plot(assignmentOnNonXAxisVariables, newXm);
+    System.out.println(graphPlot);
+  }
+
+  // Throws a class cast exception
+//  @Test
   public void testArrayOutOfBound() {
     String model =
         ""
